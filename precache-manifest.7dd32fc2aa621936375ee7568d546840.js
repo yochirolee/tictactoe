82ceb400b8670e05e45b457f05bae8da62668625
yochirolee <yleecruz@gmail.com>
2020-06-02 20:14:33 -0500

    Updates

---
+++ precache-manifest.7dd32fc2aa621936375ee7568d546840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6793523d5d3db5b661604c530f03d6f",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "f15b2ad25e24c042a298",
+    "url": "/tictactoe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6885805e17fda6bd7722",
+    "url": "/tictactoe/static/css/main.a0b74c84.chunk.css"
+  },
+  {
+    "revision": "f15b2ad25e24c042a298",
+    "url": "/tictactoe/static/js/2.cf4b1bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.cf4b1bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6885805e17fda6bd7722",
+    "url": "/tictactoe/static/js/main.d62c72b7.chunk.js"
+  },
+  {
+    "revision": "f8965624717357d3862c",
+    "url": "/tictactoe/static/js/runtime-main.4281d3e9.js"
+  }
+]);
